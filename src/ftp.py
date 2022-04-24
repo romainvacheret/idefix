cbff3f3d5f9367a4d40afef1c6b11e1169374816
Romain Vacheret <romain.vacheret@gmail.com>
2022-04-24 11:48:52 +0200

    feat: add ftp server

---
+++ src/ftp.py
@@ -0,0 +1,35 @@
+from os import getcwd, mkdir
+from os.path import join, isdir
+from typing import Final, NoReturn
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+SERVER_HOST: Final = '127.0.0.1'
+SERVER_PORT: Final = 21
+SERVER_USER: Final = 'idefix'
+SERVER_PSWD: Final = 'idefix'
+FOLDER_PATH: Final = 'projects'
+
+
+class FTPServerWrapper:
+	def __init__(self) -> None:
+		self._authorizer = DummyAuthorizer()
+		self._handler = FTPHandler
+		self._handler.authorizer = self._authorizer
+		self._server = FTPServer((SERVER_HOST, SERVER_PORT), self._handler)
+		self._folder_full_path = join(getcwd(), FOLDER_PATH)
+
+		self._create_folder()
+		self._authorizer.add_user(SERVER_USER,
+			SERVER_PSWD,
+			self._folder_full_path,
+			perm='elradfmwMT')
+
+	def _create_folder(self) -> None:
+		if not isdir(self._folder_full_path):
+			mkdir(self._folder_full_path)
+
+	def serve(self) -> NoReturn:
+		self._server.serve_forever()
